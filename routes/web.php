27601d5675bbe0799bce2063efc7ed90566fcdfa
ChrisGarozzo <garozzochristian@gmail.com>
2024-02-09 17:58:27 +0100

    added scaffolding laravel

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('pages.index');
+    $name = "Christian";
+    $lastname = "Garozzo";
+
+    return view("pages.index", compact("name", "lastname"));
 });
